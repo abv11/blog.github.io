7bdd1bdbf3787720a4f4cc9eae00f5adb4cfe06b
abv11 <abv1@discente.ifpe.edu.br>
2025-02-12 10:42:52 -0300

    Delete projeto-final-LOP directory

--- projeto-final-LOP/js/anonovo.js
+++
@@ -1,9 +0,0 @@
-//Declaracão de variáveis
-let contador = 10
-
-//Estrutura de repetição
-while(contador >=0){
-  document.write(contador + "<br>")
-  contador-- //isso é o mesmo que contador = contador - 1
-}
-document.write("Feliz Ano Novo!")
--- projeto-final-LOP/js/somatorio.js
+++
@@ -1,4 +0,0 @@
-let n = parseInt(prompt("Digite um número inteiro:")); //O usuário vai digitar o número desejado
-const soma = (n * (n + 1)) / 2; //O programa vai calcular a soma dos números naturais  não nulos até o número digitado
-
-document.write("<br><br><br><br><br><br><br><br>O resultado da soma dos ℕ* de 1 até " + n + " é: " + soma); //O programa vai exibir o resultado da soma na tela
